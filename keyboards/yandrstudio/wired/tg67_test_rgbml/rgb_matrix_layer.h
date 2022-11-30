b4a3d4546dfae9e9cd0064d972bff4a53f5b5658
jiaxin96 <572673807@qq.com>
2022-12-01 01:53:14 +0800

    完善rgb matrix layer

--- keyboards/yandrstudio/wired/tg67_test_rgbml/rgb_matrix_layer.h
+++ keyboards/yandrstudio/wired/tg67_test_rgbml/rgb_matrix_layer.h
@@ -7,8 +7,8 @@
 #include <stdbool.h>
 
 typedef struct {
-    uint8_t index; // The first LED to light
-    uint8_t count; // The number of LEDs to light
+    uint8_t index;  // The first LED to light
+    uint8_t count;  // The number of LEDs to light
     uint8_t hue;
     uint8_t sat;
     uint8_t val;
@@ -18,12 +18,13 @@ typedef uint32_t rgb_matrix_adv_layers_mask_t;
 
 extern rgb_matrix_adv_layer_segment_t const *const *rgb_matrix_layers;
 
-
-#define RGB_MATRIX_MAX_LAYERS 2
 #define RGB_MATRIX_END_SEGMENT_INDEX (255)
-#define RGB_MATRIX_END_SEGMENT {RGB_MATRIX_END_SEGMENT_INDEX,0,0,0,0}
-#define RGB_MATRIX_LAYER_SEGMENTS(...) { __VA_ARGS__, RGB_MATRIX_END_SEGMENT }
-#define RGB_MATRIX_LAYERS_LIST(...) { __VA_ARGS__, NULL }
+#define RGB_MATRIX_END_SEGMENT \
+    { RGB_MATRIX_END_SEGMENT_INDEX, 0, 0, 0, 0 }
+#define RGB_MATRIX_LAYER_SEGMENTS(...) \
+    { __VA_ARGS__, RGB_MATRIX_END_SEGMENT }
+#define RGB_MATRIX_LAYERS_LIST(...) \
+    { __VA_ARGS__, NULL }
 
 #ifndef RGB_MATRIX_LAYERS_OVERRIDE_RGB_OFF
 #    define RGB_MATRIX_LAYERS_OVERRIDE_RGB_OFF
@@ -33,6 +34,9 @@ extern rgb_matrix_adv_layer_segment_t const *const *rgb_matrix_layers;
 #    define RGB_MATRIX_LAYERS_RETAIN_VAL
 #endif
 
+#ifndef RGB_MATRIX_MAX_LAYERS
+#    define RGB_MATRIX_MAX_LAYERS 8
+#endif
 
 void rgb_matrix_adv_set_layer_state(uint8_t layer, bool enabled);
 void rgb_matrix_adv_blink_layer_repeat(uint8_t layer, uint16_t duration_ms, uint8_t times);
