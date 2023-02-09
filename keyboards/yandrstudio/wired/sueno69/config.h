951c726e25bfbfcc815aadf71a7c42ce9dd10209
jiaxin96 <572673807@qq.com>
2023-02-10 05:01:41 +0800

    add sueno69

--- keyboards/yandrstudio/wired/sueno69/config.h
+++ keyboards/yandrstudio/wired/sueno69/config.h
@@ -1,18 +1,5 @@
-/* Copyright 2022 JasonRen(biu)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright 2022 Y&R-Biu (@jiaxin96)
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
@@ -23,7 +10,7 @@
 #define MATRIX_COLS 16
 
 /* key matrix pins */
-//                        0    1   2   3  4  5   6   7   8  9  10 11  12  13 14   
+//                        0    1   2   3  4  5   6   7   8  9  10 11  12  13 14
 #define MATRIX_COL_PINS { B10,B11,B12,A9,B13,B14,B15,B6,B5,A8,A10,B3,B4,A15,A0,B9}
 #define MATRIX_ROW_PINS { A3,A4,A7,A6,A5}
 
@@ -32,11 +19,7 @@
 
 #define TAP_CODE_DELAY 15
 
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
-
-#define BOOTMAGIC_LITE_ROW 0
-#define BOOTMAGIC_LITE_COLUMN 0
+#define TAP_HOLD_CAPS_DELAY 250
 
 #ifdef RGBLIGHT_ENABLE
 #    define RGB_DI_PIN B7
@@ -47,8 +30,22 @@
 #    define RGBLIGHT_LIMIT_VAL 255
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED true
 #    define RGBLIGHT_ANIMATIONS
-#endif
 
-#       define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 3
-#       define DYNAMIC_KEYMAP_LAYER_COUNT 4
-#       define FEE_PAGE_COUNT 4
+#    define RGBLIGHT_EFFECT_BREATHING
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define RGBLIGHT_EFFECT_SNAKE
+#    define RGBLIGHT_EFFECT_KNIGHT
+#    define RGBLIGHT_EFFECT_CHRISTMAS
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    define RGBLIGHT_EFFECT_RGB_TEST
+#    define RGBLIGHT_EFFECT_ALTERNATING
+#    define RGBLIGHT_EFFECT_TWINKLE
+#    define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_MOOD
+
+#    define WS2812_PWM_DRIVER PWMD4  // default: PWMD2
+#    define WS2812_PWM_CHANNEL 2  // default: 2
+#    define WS2812_DMA_STREAM STM32_DMA1_STREAM7  // DMA Stream for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
+#    define WS2812_DMA_CHANNEL 7  // DMA Channel for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
+
+#endif
