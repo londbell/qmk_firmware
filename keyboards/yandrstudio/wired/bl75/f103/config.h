c92cef5a730605004ae59575be3e332d2e19ac22
jiaxin96 <572673807@qq.com>
2023-04-02 14:02:12 +0800

    更新bl75 添加生产测试固件

--- keyboards/yandrstudio/wired/bl75/f103/config.h
+++ keyboards/yandrstudio/wired/bl75/f103/config.h
@@ -15,8 +15,7 @@
  */
 #pragma once
 
-/* RGB Matrix */
-#ifdef RGB_MATRIX_ENABLE
+#ifdef WS2812_DRIVER_PWM
 #    define WS2812_PWM_DRIVER PWMD3  // default: PWMD2
 #    define WS2812_PWM_CHANNEL 2  // default: 2
 #    define WS2812_DMA_STREAM STM32_DMA1_STREAM3  // DMA Stream for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
