6145c96b703f10b6bb28c57c077cc27eb954b6d1
JX <572673807@qq.com>
2023-05-16 19:36:22 +0800

    up tg67

--- keyboards/yandrstudio/wired/tg67/rules.mk
+++ keyboards/yandrstudio/wired/tg67/rules.mk
@@ -15,7 +15,7 @@ NKRO_ENABLE = yes           # USB Nkey Rollover
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 
-CUSTOM_MATRIX = lite    # for using the A9 pin as matrix io
-QUANTUM_SRC += matrix.c
+# CUSTOM_MATRIX = lite    # for using the A9 pin as matrix io
+# QUANTUM_SRC += matrix.c
 
-NO_USB_STARTUP_CHECK := yes
+# NO_USB_STARTUP_CHECK := yes
