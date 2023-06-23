232a11bba7fe2795b746dfa71c0c4631081c5fa6
jiaxin96 <572673807@qq.com>
2023-06-23 18:26:29 +0800

    up man40 keymap

--- keyboards/yandrstudio/wired/man40/keymaps/via/keymap.c
+++ keyboards/yandrstudio/wired/man40/keymaps/via/keymap.c
@@ -19,9 +19,9 @@ enum keyboard_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_F1,  KC_INS,  KC_HOME, KC_PGUP,  KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
-        KC_F2,  KC_DEL,  KC_END,  KC_PGDN,  KC_P7,  KC_P8,   KC_P9,   KC_PPLS,
-        KC_F3,  KC_MUTE, KC_VOLU, KC_VOLD,  KC_P4,  KC_P5,   KC_P6,   KC_PPLS,
+        KC_F1,  KC_PSCR, KC_SCRL, KC_PAUS,  KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
+        KC_F2,  KC_INS,  KC_HOME, KC_PGUP,  KC_P7,  KC_P8,   KC_P9,   KC_PPLS,
+        KC_F3,  KC_DEL,  KC_END,  KC_PGDN,  KC_P4,  KC_P5,   KC_P6,   KC_PPLS,
         KC_F4,  KC_STOP, KC_UP,   KC_MPLY,  KC_P1,  KC_P2,   KC_P3,   KC_PENT,
         KC_F5,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_P0,  KC_P0,   KC_PDOT, KC_PENT),
     [1] = LAYOUT(
