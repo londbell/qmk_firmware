5e208a7c77f2b2b961da00642f9f998ce78505d8
jiaxin96 <572673807@qq.com>
2023-07-02 18:06:13 +0800

    up studiox

--- keyboards/yandrstudio/wired/studiox65/f103/mcuconf.h
+++ keyboards/yandrstudio/wired/studiox65/f103/mcuconf.h
@@ -5,13 +5,8 @@
 
 #include_next <mcuconf.h>
 
-#if (PRODUCT_YR_ID == 3) || (PRODUCT_YR_ID == 2)
-#   undef STM32_PWM_USE_TIM2
-#   define STM32_PWM_USE_TIM2 TRUE
-#else
 #   undef STM32_PWM_USE_TIM3
 #   define STM32_PWM_USE_TIM3 TRUE
-#endif
 
 #undef  STM32_PLLXTPRE
 #define STM32_PLLXTPRE STM32_PLLXTPRE_DIV2
