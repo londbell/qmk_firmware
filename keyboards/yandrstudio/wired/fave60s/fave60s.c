ed81955bf43137d2ddbbe4457979cb4850715b45
jiaxin96 <572673807@qq.com>
2023-08-24 16:47:01 +0800

    up lin60

--- keyboards/yandrstudio/wired/fave60s/fave60s.c
+++ keyboards/yandrstudio/wired/fave60s/fave60s.c
@@ -2,3 +2,13 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "fave60s.h"
+
+void eeconfig_init_kb(void) {
+#ifdef BACKLIGHT_ENABLE
+    backlight_enable();
+    backlight_level(5);
+#endif
+
+    eeconfig_update_kb(0);
+    eeconfig_init_user();
+}
